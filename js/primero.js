7a72354ef2869a3ff5abb5461215485b11bdd507
maxichaio <mchaio@udesa.edu.ar>
2020-05-29 13:37:05 -0300

    primer intento de js

---
+++ js/primero.js
@@ -0,0 +1,39 @@
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0')
+    .then(function(respuesta){
+        return respuesta.json();
+    })
+    .then(function(datos){
+        let albums = datos.albums.data;
+        console.log(albums)
+        let albumsContenedor = document.querySelector('#albumesjs');
+        for (const albumlista of albums){
+            albumsContenedor.innerHTML += ` <div>
+            <a href="mantra.html">
+            <img class="imagen" src="${albumlista.cover_xl}" alt=""></a>
+            <h4>${albumlista.title}</h4>
+        </div>`
+            }
+        let artistas = datos.artists.data;
+        let artistasContenedor = document.querySelector ('#artistasjs');
+        for (const artistaslista of artistas) {
+            artistasContenedor.innerHTML += `<div>
+            <a href="BadBunny.html">
+            <img class="imagen" src="${artistaslista.picture_xl}" alt=""></a>
+            <h4>${artistaslista.name}</h4>
+            </div>`
+            
+        }
+        let tracks = datos.tracks.data;
+        let tracksContenedor = document.querySelector ('#tracksjs');
+        for (const trackslista of tracks) {
+            tracksContenedor.innerHTML += `<div>
+            <a href="dailymix.html">
+            <img class="imagen" src="${trackslista.preview}" alt=""></a>
+            <h4>${trackslista.title}</h4>
+            </div>`
+            
+        }
+    })
+        .catch(function(error){
+            console.error(error);
+        })
